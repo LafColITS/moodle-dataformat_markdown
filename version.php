c7e0564fd953364780c8e020343a5ecdacb54aa2
Charles Fulton <fultonc@lafayette.edu>
2016-06-23 11:50:14 -0400

    Markdown dataformat v1.1.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'dataformat_markdown';
-$plugin->version   = 2016042900;
+$plugin->version   = 2016062300;
 $plugin->requires  = 2016052300;  // Requires this Moodle version.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.0';
+$plugin->release   = 'v1.1.0';
